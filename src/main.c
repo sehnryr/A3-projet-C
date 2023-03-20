909a7fdcfc0b1b47ce76769004f9b5889910ca29
sehnryr <youn@melois.dev>
2023-03-20 18:12:50 +0100

    add basic main

--- src/main.c
+++ src/main.c
@@ -4,5 +4,18 @@
 
 int main()
 {
+    // Températures initiales (valeurs par défaut du simulateur)
+    temp_t temperature;
+    temperature.exterieure = 10.0;
+    temperature.interieure = 20.0;
+
+    // Création du simulateur (puissance initialement à 0%)
+    struct simParam_s *simulateur_ps = simConstruct(temperature);
+
+    
+
+    // Destruction du simulateur
+    simDestruct(simulateur_ps);
+
     return EXIT_SUCCESS;
 }
