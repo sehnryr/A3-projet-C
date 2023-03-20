86788a1a97e8f5a36e68978938b275335a9dbdb2
sehnryr <youn@melois.dev>
2023-03-20 20:31:48 +0100

    simplify zipping script

--- sources.sh
+++ sources.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
-zip sources_AA_YM.zip \
-    include/consigne.h \
-    include/define.h \
-    include/regulation.h \
-    include/visualisationC.h \
-    include/visualisationT.h \
-    src/consigne.c \
-    src/regulation.c \
-    src/visualisationC.c \
-    src/visualisationT.c \
-    test/test_sim.c \
+zip -r sources_AA_YM.zip \
+    include \
+    src \
+    test \
     Makefile
