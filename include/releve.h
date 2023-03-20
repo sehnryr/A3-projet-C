89bd9734fcd49440fe33e0aa4d09f03d2bf7452a
sehnryr <youn@melois.dev>
2023-03-20 23:13:08 +0100

    add template for releve

--- include/releve.h
+++ include/releve.h
@@ -1,6 +1,9 @@
+#include "ftd2xx.h"
+#include "define.h"
+
 #ifndef RELEVE_H
 #define RELEVE_H
 
-
+temp_t releve(FT_HANDLE ftHandle);
 
 #endif // RELEVE_H
--- src/releve.c
+++ src/releve.c
@@ -1,1 +1,12 @@
 #include "releve.h"
+
+/**
+ * @brief Releve la temperature via la liaison USB
+ * 
+ * @param ftHandle Le handle de la liaison USB
+ * @return temp_t La temperature interieure et exterieure relevee
+ */
+temp_t releve(FT_HANDLE ftHandle)
+{
+    // TODO: Implementer la fonction
+}
