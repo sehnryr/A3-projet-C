0d8bd3d340e73030a3b2ef256008d855ea403dbe
sehnryr <youn@melois.dev>
2023-03-20 11:59:21 +0100

    initial commit for regulation

--- include/regulation.h
+++ include/regulation.h
@@ -5,6 +5,18 @@
 #ifndef REGULATION_H
 #define REGULATION_H
 
+#define Kp 1.0
+#define Ki 0.1
+#define Kd 0.1
+
+/// Type d'une consigne
+enum REGULATION_TYPE
+{
+    TOUT_OU_RIEN = 1, /// Tout ou rien
+    PID = 2           /// Proportionnel Intégral Dérivé
+};
+
+float regulation(int regul, float temp, float consigne);
 float regulationTest(int regul, float consigne, float *tabT, int nT);
 
 #endif // REGULATION_H
--- src/regulation.c
+++ src/regulation.c
@@ -2,9 +2,39 @@
 #include <stdlib.h>
 #include "regulation.h"
 
+/**
+ * @brief Fonction de régulation
+ *
+ * @param regul Type de régulation
+ * @param temp Température intérieure
+ * @param consigne Consigne de température intérieure à atteindre
+ */
+float regulation(int regul, float temp, float consigne)
+{
+    switch (regul)
+    {
+    case TOUT_OU_RIEN:
+        // Si la température est inférieure à la consigne,
+        // on allume le chauffage à 40%. Sinon, on l'éteint.
+        if (temp < consigne)
+            return 0.4;
+        else
+            return 0;
+    case PID:
+        /* TODO: Implémentation PID */
+    default:
+        return 0;
+    }
+}
+
 float regulationTest(int regul, float consigne, float *tabT, int nT)
 {
-    float cmd;
+    float cmd = 100.0;
+
+    for (int i = 0; i < nT; ++i)
+    {
+        cmd = regulation(regul, tabT[i], consigne);
+    }
 
     return cmd;
 }
