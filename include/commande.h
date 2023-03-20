09dd6229356f23a54677d58640d29a2e9b9cd028
sehnryr <youn@melois.dev>
2023-03-20 23:13:03 +0100

    add template for commande

--- include/commande.h
+++ include/commande.h
@@ -1,6 +1,8 @@
+#include "ftd2xx.h"
+
 #ifndef COMMANDE_H
 #define COMMANDE_H
 
-
+void commande(FT_HANDLE ftHandle, float thermostat_f);
 
 #endif // COMMANDE_H
--- src/commande.c
+++ src/commande.c
@@ -1,1 +1,12 @@
 #include "commande.h"
+
+/**
+ * @brief Envoie la commande de chauffage via la liaison USB
+ *
+ * @param ftHandle Le handle de la liaison USB
+ * @param thermostat_f La consigne de chauffage
+ */
+void commande(FT_HANDLE ftHandle, float thermostat_f)
+{
+    // TODO: Implementer la fonction
+}
