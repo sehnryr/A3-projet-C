c42672de3a294d5fb78fbf89a4baaa82060ce08b
sehnryr <youn@melois.dev>
2023-03-20 19:59:57 +0100

    redo documentation for consigne

--- src/consigne.c
+++ src/consigne.c
@@ -5,52 +5,47 @@
  * @brief Récupère la consigne de température
  *
  * @param thermostatPrec_f Ancienne consigne
- * @return float Consigne de température interieure
+ * @return Consigne de température interieure
  */
 float consigne(float thermostatPrec_f)
 {
+    // Tentative de lecture de consigne.txt
 
-   float thermostat_f = 0;
+    // Si le fichier .verrouConsigne existe, le fichier consigne.txt est en 
+    // cours d'utilisation par un autre programme, retour de la fonction.
+    if (access(".verrouConsigne", F_OK) != -1)
+    {
+        // printf("Impossible d'ouvrir consigne.txt, le fichier est en cours d'utilisation.\n");
+        return;
+    }
+    // Sinon, création d'un fichier .verrouConsigne qui indique que le fichier
+    // consigne.txt est en cours de lecture par ce programme.
+    else
+    {
+        FILE *fp2 = fopen(".verrouConsigne", "w");
+        fclose(fp2);
+    }
 
-   // j'essaye de lire le fichier "consigne.txt"
+    // Lecture du fichier consigne.txt
+    FILE *fp = fopen("consigne.txt", "r");
 
-   // si le fichier .verrouConsigne existe, le fichier "consigne.txt" est en cours d'utilisation donc je quitte la fonction
-   // la fonction access renvoie -1 si le fichier n'existe pas
-   if (access(".verrouConsigne", F_OK) != -1)
-   {
-      printf("Impossible d'ouvrir consigne.txt, le fichier est en cours d'utilisation.\n");
-      return thermostatPrec_f;
-   }
-   // s'il n'y en a pas qui existe, je crée un fichier .verrouConsigne qui indique que le fichier "consigne.txt" est en cours de lecture
-   else
-   {
-      FILE *fp2;
-      fp2 = fopen(".verrouConsigne", "w");
-      fclose(fp2);
-   }
+    // Si le fichier consigne.txt n'existe pas, on quitte le programme.
+    if (fp == NULL)
+    {
+        printf("Impossible d'ouvrir consigne.txt, le programme va se fermer \n");
+        exit(1);
+    }
 
-   // je crée un pointeur de type FILE pour lire le fichier "consigne.txt"
-   FILE *fp;
+    float thermostat_f = 0; // Consigne de température
 
-   // je lis le fichier "consigne.txt"
-   fp = fopen("consigne.txt", "r");
+    // Lecture de la consigne de température dans le fichier consigne.txt
+    fscanf(fp, "%f", &thermostat_f);
+    fclose(fp); // Fermeture du fichier consigne.txt après lecture
 
-   // si je n'arrive pas à ouvrir le fichier "consigne.txt", je quitte le programme
-   if (fp == NULL)
-   {
-      printf("Impossible d'ouvrir consigne.txt, le programme va se fermer \n");
-      exit(1);
-   }
+    // Suppression du fichier .verrouConsigne après manipulation du fichier
+    // consigne.txt
+    remove(".verrouConsigne");
 
-   // je lis le fichier "consigne.txt" et je stocke la valeur obtenu dans la variable thermostat_f
-   fscanf(fp, "%f", &thermostat_f);
-
-   // je ferme le fichier "consigne.txt"
-   fclose(fp);
-
-   // je supprime le fichier .verrouConsigne car je ne l'utilise plus
-   remove(".verrouConsigne");
-
-   // je retourne la valeur de la consigne
-   return thermostat_f;
+    // Retourne la consigne de température
+    return thermostat_f;
 }
