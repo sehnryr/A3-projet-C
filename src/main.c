6b217bd76c9a45a4f9025f1965cb77db241b7a46
Alexandre-github-code <alexandre.auguste@isen-ouest.yncrea.fr>
2023-03-20 15:19:22 +0100

    oups

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    float score1 = 0, score2 = 0, score3 = 0;
+    float score1 = 0, score2 = 0, score3 = 0, score4=0, score5=0;
     score1 = testVisualisationT();
     score2 = testConsigne();
     score3 = testVisualisationC();
@@ -11,5 +11,8 @@ int main()
     printf("testVisualisationT\t : score = % g % %\n", score1 * 100);
     printf("testConsigne \t\t : score = % g % %\n", score2 * 100);
     printf("testVisualisationC\t : score = % g % %\n", score3 * 100);
+    printf("testRegulationTOR\t : score = % g % %\n",score4*100);
+    printf("testRegulationPID\t : score = % g % %\n",score5*100);
+
     return EXIT_SUCCESS;
 }
