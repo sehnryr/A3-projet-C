a7e427dc1a1644cfd21bcfb2509bc11bc8c49109
Alexandre-github-code <alexandre.auguste@isen-ouest.yncrea.fr>
2023-03-20 15:17:38 +0100

    debug

--- src/visualisationT.c
+++ src/visualisationT.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include "visualisationT.h"
 
 void visualisationT(temp_t myTemp)
@@ -5,17 +6,17 @@ void visualisationT(temp_t myTemp)
     // j'essaye de lire le fichier data.txt
     
     // si le fichier .verrouData existe, le fichier data.txt est en cours d'utilisation donc je quitte la fonction
-    // la fonction fopen renvoie NULL si le fichier n'existe pas
-    if(fopen("../.verrouData", "r")==NULL) 
+    // la fonction access renvoie -1 si le fichier n'existe pas
+    if(access(".verrouData", F_OK) != -1) 
     {
-        printf("Impossible d'ouvrir data.txt, le fichier est en cours d'utilisation. Le programme va se fermer. \n");
+        printf("Impossible d'ouvrir data.txt, le fichier est en cours d'utilisation.\n");
         return;
     }
     // s'il n'y en a pas qui existe, je crée un fichier .verrouData qui indique que le fichier data.txt est en cours de lecture
     else 
     {
         FILE *fp2;
-        fp2 = fopen("../.verrouData", "w");
+        fp2 = fopen(".verrouData", "w");
         fclose(fp2);
     }
         
@@ -23,18 +24,17 @@ void visualisationT(temp_t myTemp)
     FILE *fp;
 
     // je lis le fichier data.txt
-    fp = fopen("../data.txt", "r");
+    fp = fopen("data.txt", "r");
 
     // si je n'arrive pas à ouvrir le fichier data.txt, je quitte le programme
     if (fp == NULL)
     {
-        printf("Impossible d'ouvrir data.txt, le programme va se fermer \n");
+        printf("Impossible d'ouvrir data.txt, le programme va se fermer. \n");
         exit(1);
     }
     
-
     // je crée une variable de type char qui alterne entre true et false pour stocker le témoin de chauffe stockée à la premiere ligne du fichier data.txt
-    char tChauffe[5];
+    char tChauffe[8];
 
     // je crée une variable de type float pour stocker la température extérieure stockée à la deuxieme ligne du fichier data.txt, ici elle ne me sert à rien mais je la laisse pour que le code soit plus clair car je l'utilise dans visualisationC.c. Cela peut aussi me permettre de l'afficher si j'en ai besoin (pour débuguer ou voir son évolution). 
     float tempExt; 
@@ -49,14 +49,14 @@ void visualisationT(temp_t myTemp)
     fclose(fp);
 
     // j'ouvre le fichier data.txt en mode écriture
-    fp = fopen("../data.txt", "w");
+    fp = fopen("data.txt", "w");
 
     // je stocke les valeurs pris en argument dans la fonction dans le fichier data.txt, ainsi que le témoin de chauffe qui ne change pas
-    fprintf(fp, "%s %f %f", tChauffe, myTemp.exterieure, myTemp.interieure);
+    fprintf(fp, "%s\n%f\n%f\n", tChauffe, myTemp.interieure, myTemp.exterieure);
 
-    // je ferme le fichier data.txt
+    // je ferme le fichier "data.txt"
     fclose(fp);
 
     // je supprime le fichier .verrouData car je ne l'utilise plus
-    remove("../.verrouData");
+    remove(".verrouData");
 }
