8f50b64b50f6403bc75a3f4369fca53fda9e5ab3
sehnryr <youn@melois.dev>
2023-03-20 19:16:16 +0100

    complete main with simulation

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "consigne.h"
+#include "regulation.h"
 #include "simulateur.h"
+#include "visualisationC.h"
+#include "visualisationT.h"
 
 int main()
 {
@@ -12,10 +16,51 @@ int main()
     // Création du simulateur (puissance initialement à 0%)
     struct simParam_s *simulateur_ps = simConstruct(temperature);
 
-    
+    float thermostat = 0; // Consigne de température
+    float puissance = 0;  // Puissance de chauffage
+
+    // Régulation PID
+    const int regul = PID;      // Type de régulation
+    float PID_old_consigne = 0; // Ancienne consigne
+    float PID_old_e = 0;        // Ancien écart
+    float PID_I = 0;            // Terme intégral
+
+    // Boucle de régulation
+    while (1)
+    {
+        // Récupération de la consigne
+        thermostat = consigne(thermostat);
+
+        if (thermostat <= 5)
+        {
+            break;
+        }
+
+        // Régulation PID
+        puissance = regulation(
+            regul,
+            temperature.interieure,
+            thermostat,
+            &PID_old_consigne,
+            &PID_old_e,
+            &PID_I);
+
+        // Calcul de la température
+        temperature = simCalc(puissance, simulateur_ps);
+
+        // Visualisation de la consigne
+        visualisationC(puissance);
+
+        // Visualisation de la température
+        visualisationT(temperature);
+    }
 
     // Destruction du simulateur
     simDestruct(simulateur_ps);
+
+    // Mise de la puissance à 0 pour arrêter le chauffage
+    puissance = 0;
+    visualisationC(puissance);
 
     return EXIT_SUCCESS;
 }
