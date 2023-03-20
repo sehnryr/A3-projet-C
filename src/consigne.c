edbb01ff21fdb463dcb068ceab5c92e2ab3d4218
Alexandre-github-code <alexandre.auguste@isen-ouest.yncrea.fr>
2023-03-20 15:17:15 +0100

    debug

--- src/consigne.c
+++ src/consigne.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include "consigne.h"
 
 float consigne(float thermostatPrec_f)
@@ -8,17 +9,17 @@ float consigne(float thermostatPrec_f)
    // j'essaye de lire le fichier "consigne.txt"
 
    // si le fichier .verrouConsigne existe, le fichier "consigne.txt" est en cours d'utilisation donc je quitte la fonction
-   // la fonction fopen renvoie NULL si le fichier n'existe pas
-   if (fopen("../.verrouConsigne", "r") == NULL)
+   // la fonction access renvoie -1 si le fichier n'existe pas
+   if (access(".verrouConsigne", F_OK) != -1)
    {
-      printf("Impossible d'ouvrir consigne.txt, le fichier est en cours d'utilisation. Le programme va se fermer. \n");
+      printf("Impossible d'ouvrir consigne.txt, le fichier est en cours d'utilisation.\n");
       return thermostatPrec_f;
    }
    // s'il n'y en a pas qui existe, je crée un fichier .verrouConsigne qui indique que le fichier "consigne.txt" est en cours de lecture
    else
    {
       FILE *fp2;
-      fp2 = fopen("../.verrouConsigne", "w");
+      fp2 = fopen(".verrouConsigne", "w");
       fclose(fp2);
    }
 
@@ -26,7 +27,7 @@ float consigne(float thermostatPrec_f)
    FILE *fp;
 
    // je lis le fichier "consigne.txt"
-   fp = fopen("../consigne.txt", "r");
+   fp = fopen("consigne.txt", "r");
 
    // si je n'arrive pas à ouvrir le fichier "consigne.txt", je quitte le programme
    if (fp == NULL)
@@ -36,13 +37,13 @@ float consigne(float thermostatPrec_f)
    }
 
    // je lis le fichier "consigne.txt" et je stocke la valeur obtenu dans la variable thermostat_f
-   fscanf(fp, "%f %f", &thermostat_f);
+   fscanf(fp, "%f", &thermostat_f);
 
    // je ferme le fichier "consigne.txt"
    fclose(fp);
 
    // je supprime le fichier .verrouConsigne car je ne l'utilise plus
-   remove("../.verrouConsigne");
+   remove(".verrouConsigne");
 
    // je retourne la valeur de la consigne
    return thermostat_f;
